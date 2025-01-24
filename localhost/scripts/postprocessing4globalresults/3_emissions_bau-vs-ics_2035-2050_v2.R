0061fcbf3a3dcc274de313ae90b0dfa019a8fbbb
ricmofuss <ricardodrrp@gmail.com>
2025-01-24 03:13:01 -0600

    d

--- localhost/scripts/postprocessing4globalresults/3_emissions_bau-vs-ics_2035-2050_v2.R
+++ localhost/scripts/postprocessing4globalresults/3_emissions_bau-vs-ics_2035-2050_v2.R
@@ -12,7 +12,7 @@
 # 6.- Ideally compare tet a tet with Demand scripts to align homolgous chunks
 # 7.- LINEA 229 scenario.list <- c("BaU") BORRAR!!!
 # 8.- Check for Linux 
-# 9.- Neighboring countries
+# 9.- Neighboring countries when continent or region is selected, something with the croping vect layer, simplified?
 
 # Internal parameters ----
 string_pattern_yes <- "mwi_1000m" #String pattern to be searched when selecting folders for the rasters' geocomputation
