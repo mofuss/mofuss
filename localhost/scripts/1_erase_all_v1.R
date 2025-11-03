a7a71137733700b25be2263e5e5b20f85934c902
adrianghilardi <ghilardi.adrian@gmail.com>
2025-11-03 01:28:57 -0600

    new erase all

--- localhost/scripts/1_erase_all_v1.R
+++ localhost/scripts/1_erase_all_v1.R
@@ -1,5 +1,5 @@
 # Copyright 2025 Stockholm Environment Institute ----
-
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -19,7 +19,7 @@
 
 # Internal parameters ----
 # # Select MoFuSS platform:
-# webmofuss = 1 # "1" is  web-MoFuSS running in our Ubuntu server, "0" is localcal host (Windows or Linux)
+# webmofuss = 1 # "1" is  web-MoFuSS running in our Ubuntu server, "0" is localhost (Windows or Linux)
 # source(paste0(scriptsmofuss,"00_webmofuss.R"))
 
 # Load libraries ----
@@ -30,7 +30,21 @@ os <- Sys.info()["sysname"]
 # Set working directory
 setwd(countrydir)
 
-# Define directories and patterns to clean ####
+# ------------------------------------------------------------
+# Define base paths
+# ------------------------------------------------------------
+base_path <- file.path("LULCC", "DownloadedDatasets", paste0("SourceData", country_name))
+
+# Use /demand100m/ if it exists, otherwise /demand/
+demand_base <- if (dir.exists(file.path(base_path, "demand100m"))) {
+  file.path(base_path, "demand100m")
+} else {
+  file.path(base_path, "demand")
+}
+
+# ------------------------------------------------------------
+# Define directories to remove
+# ------------------------------------------------------------
 directories_to_remove <- c(
   "Debugging", "debug*", "norm*", 
   "HTML_animation_OutBaU", "HTML_animation_OutICS", 
@@ -40,16 +54,27 @@ directories_to_remove <- c(
   "LULCC/TempRaster", "LULCC/TempTables", "LULCC/TempVector", 
   "LULCC/TempVector_GCS", "LULCC/Wizard_imgs",
   "LULCC/lucdynamics_luc1", "LULCC/lucdynamics_luc2", "LULCC/lucdynamics_luc3",
-  paste0("LULCC/DownloadedDatasets/SourceData",country_name,"/demand100m/demand_out"),
-  paste0("LULCC/DownloadedDatasets/SourceData",country_name,"/demand100m/demand_temp"),
-  paste0("LULCC/DownloadedDatasets/SourceData",country_name,"/demand100m/pop_maps_byregion"),
-  paste0("LULCC/DownloadedDatasets/SourceData",country_name,"/demand100m/pop_out"),
-  paste0("LULCC/DownloadedDatasets/SourceData",country_name,"/demand100m/pop_temp"),
-  paste0("LULCC/DownloadedDatasets/SourceData",country_name,"/demand100m/to_idw"),
-  paste0("LULCC/DownloadedDatasets/SourceData",country_name,"/inTables"),
-  paste0("LULCC/DownloadedDatasets/SourceData",country_name,"/inVector")
+  file.path(demand_base, "demand_out"),
+  file.path(demand_base, "demand_temp"),
+  file.path(demand_base, "pop_maps_byregion"),
+  file.path(demand_base, "pop_out"),
+  file.path(demand_base, "pop_temp"),
+  file.path(demand_base, "to_idw"),
+  file.path(base_path, "inTables"),
+  file.path(base_path, "inVector")
 )
 
+# Expand wildcards and keep only existing paths
+expanded_dirs <- unique(unlist(lapply(directories_to_remove, Sys.glob)))
+existing_dirs <- expanded_dirs[file.exists(expanded_dirs)]
+
+# Show what will be deleted
+cat("Deleting the following directories:\n")
+cat(paste(existing_dirs, collapse = "\n"), "\n")
+
+# ------------------------------------------------------------
+# Define file patterns to remove
+# ------------------------------------------------------------
 file_patterns_to_remove <- c(
   "*.Rout", "*.txt", "*.log", "*.aux", "*.lof",
   "*.lot", "*.out", "*.toc", "*.R", "*.egoml"
@@ -60,19 +85,28 @@ latex_patterns_to_remove <- c(
   "LaTeX//SimLength.txt", "LaTeX//MCruns.txt"
 )
 
+# detect demand folder level for LULCC patterns too
+lulcc_demand_path <- demand_base  # same auto-detected path
+
 lulcc_patterns_to_remove <- c(
   "LULCC//*.Rout", "LULCC//*.csv", "LULCC//*.egoml", 
   "LULCC//*.bat", "LULCC//*.sh",
-  paste0("LULCC/DownloadedDatasets/SourceData",country_name,"/demand100m/demand_in//*.xlsx"),
-  paste0("LULCC/DownloadedDatasets/SourceData",country_name,"/demand100m/demand_in//*.csv")
+  file.path(lulcc_demand_path, "demand_in", "*.xlsx"),
+  file.path(lulcc_demand_path, "demand_in", "*.csv")
 )
 
-# Remove directories
-lapply(directories_to_remove, unlink, recursive = TRUE, force = TRUE)
+# ------------------------------------------------------------
+# Remove directories and files
+# ------------------------------------------------------------
+lapply(existing_dirs, unlink, recursive = TRUE, force = TRUE)
 
-# Remove files matching patterns
 lapply(file_patterns_to_remove, unlink, force = TRUE)
 lapply(latex_patterns_to_remove, unlink, force = TRUE)
 lapply(lulcc_patterns_to_remove, unlink, force = TRUE)
+
+cat("✅ Cleanup completed successfully.\n")
+
+# End of script ----
+
 
 # End of script ----
