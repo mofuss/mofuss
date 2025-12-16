6528f96c2aff39ed4423784e488f043268da3edc
adrianghilardi <ghilardi.adrian@gmail.com>
2025-12-16 18:31:40 +0300

    DISCAR PACKAGE SETUP

--- localhost/scripts/_setup_packages.R
+++ localhost/scripts/_setup_packages.R
@@ -1,3 +1,7 @@
+##################################################################
+##### WARNING THIS LOADS WAY TO MANY PACKAGES, FIX OR DISCARD#####
+##################################################################
+
 # Copyright 2025 Stockholm Environment Institute ----
 
 # Licensed under the Apache License, Version 2.0 (the "License");
